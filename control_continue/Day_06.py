1240c20ae18630c3d48333a4118f5ac9f3867afd
gfourny <guillaume.fourny@ynov.com>
2019-04-04 08:52:15 +0200

    Ajout correction Day_06 de la regression lineaire sur un compteur gaz

---
+++ control_continue/Day_06.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
+
+x=[[0], [2], [4], [8], [9], [10], [11], [12], [14], [17]]
+y=[[81682.0], [81720.0], [81760.0], [81826.0], [81844.0], [81864.0], [81881.0], [81900.0], [81933.0], [82003.0]]
+
+#------ Linéaire -------
+linearRegressor = LinearRegression()
+reg= linearRegressor.fit(x, y)
+
+print(linearRegressor.predict([[18]]))
+
+print("linear regression done")
+plt.subplot(211)
+plt.scatter(x, y, color = 'red')
+plt.plot(x, linearRegressor.predict(x), color = 'blue')
+plt.title("Courbe de regression linéaire")
+
+plt.subplot(212)
+plt.scatter(x, y - linearRegressor.predict(x), color = 'red')
+plt.axhline(0, color='blue')
+plt.title("Courbe des résidus")
+
+plt.show()
--- control_continue/linearRegressionGaz.py
+++ control_continue/linearRegressionGaz.py
@@ -2,13 +2,13 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
-X = np.array([[['2019-03-16', 81682], ['2019-03-18', 81720], ['2019-03-20', 81760], ['2019-03-24', 81826], ['2019-03-25', 81844], ['2019-03-26', 81864], ['2019-03-27', 81881], ['2019-03-28', 81900], ['2019-03-30', 81933], ['2019-04-03', 82003]])
+X = np.array([[0, 81682], [2, 81720], [4, 81760], [8, 81826], [9, 81844], [10, 81864], [11, 81881], [12, 81900], [14, 81933], [17, 82003]])
 # y = 1 * x_0 + 2 * x_1 + 3
 y = np.dot(X, np.array([1, 2])) + 3
 reg = LinearRegression().fit(X, y)
 reg.score(X, y)
 reg.coef_
 reg.intercept_ 
-reg.predict(np.array([[3, 5]]))
+print(reg.predict(np.array([[3, 5]])))
 
 # Calcul de regression linéaire sur un compteur gaz
