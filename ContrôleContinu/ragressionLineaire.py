96a1e4c92e0ae6096228617849f532eace309792
gfourny <guillaume.fourny@ynov.com>
2019-04-05 14:02:18 +0200

    ajout de commentaires

--- ContrôleContinu/ragressionLineaire.py
+++ ContrôleContinu/ragressionLineaire.py
@@ -6,12 +6,14 @@ from sklearn.linear_model import LogisticRegression
 x=[[0], [2], [4], [8], [9], [10], [11], [12], [14], [17]]
 y=[[81682.0], [81720.0], [81760.0], [81826.0], [81844.0], [81864.0], [81881.0], [81900.0], [81933.0], [82003.0]]
 
-#------ Linéaire -------
+#------ Regression Linéaire -------
 linearRegressor = LinearRegression()
 reg= linearRegressor.fit(x, y)
 
+#affichage du resultat du compteur prédit pour le 18ème jour
 print(linearRegressor.predict([[18]]))
 
+#Afficahge des points
 print("linear regression done")
 plt.subplot(211)
 plt.scatter(x, y, color = 'red')
@@ -23,4 +25,5 @@ plt.scatter(x, y - linearRegressor.predict(x), color = 'red')
 plt.axhline(0, color='blue')
 plt.title("Courbe des résidus")
 
+#Affichage du graphique
 plt.show()
